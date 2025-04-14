fc820d21595c32bd443544ddc53dda219a30cf28
Aavisek Choudhury <aavisek.choudhury@librespacedemo.com>
2025-04-14 22:16:17 +0530

    Average of Numbers

---
+++ ControlFlow/AverageofNumbers.py
@@ -0,0 +1,12 @@
+# Take n numbers as input and compute their average using a for loop.
+# The program should ask the user how many numbers they want to enter, and then prompt them to enter each number one by one.
+# Finally, it should calculate and display the average of those numbers.
+
+num=int(input("How many numbers do you want to enter? "))
+sum=0
+for i in range(num):
+    n=int(input("Enter number: "))
+    sum+=n
+average=sum/num 
+print("The average of the numbers is: ",average)
+
